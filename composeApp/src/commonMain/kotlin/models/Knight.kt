7ff4b55e4003ef7e9a7697a1ea7a85beb3309201
Shawn <tomatosale7774@gmail.com>
2024-02-02 20:17:03 +0900

    [feat] Knight Logic

---
+++ composeApp/src/commonMain/kotlin/models/Knight.kt
@@ -0,0 +1,56 @@
+package models
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import org.jetbrains.compose.resources.ExperimentalResourceApi
+import org.jetbrains.compose.resources.painterResource
+import utils.Side
+
+class Knight(side: Side): Piece(side, 0) {
+    @OptIn(ExperimentalResourceApi::class)
+
+    @Composable
+    override fun Image() {
+        when (side) {
+            Side.WHITE -> Image(
+                painter = painterResource("knight_white.png"),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all = 10.dp)
+            )
+            Side.BLACK -> Image(
+                painter = painterResource("knight_black.png"),
+                contentDescription = null,
+                contentScale = ContentScale.FillWidth,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(all = 10.dp)
+            )
+        }
+    }
+
+    override fun getAvaliableRoot(x: Int, y: Int, pieces: Array<Array<Piece?>>): Array<Pair<Int, Int>> {
+        var paths: Array<Pair<Int, Int>> = arrayOf()
+
+        for (add in arrayOf(Pair(1, 2), Pair(2, 1), Pair(1, -2), Pair(2, -1), Pair(-1, 2), Pair(-2, 1), Pair(-1, -2), Pair(-2, -1))) {
+            val newX = x + add.first
+            val newY = y + add.second
+
+            if (newX < 0 || newX > 7 || newY < 0 || newY > 7) continue
+
+            if (pieces[newX][newY] == null) {
+                paths += Pair(newX, newY)
+            } else if (pieces[newX][newY] != null && pieces[newX][newY]!!.side != side) {
+                paths += Pair(newX, newY)
+            }
+        }
+
+        return paths
+    }
+}
--- composeApp/src/commonMain/kotlin/models/Pawn.kt
+++ composeApp/src/commonMain/kotlin/models/Pawn.kt
@@ -1,15 +1,9 @@
 package models
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import org.jetbrains.compose.resources.ExperimentalResourceApi
